606b14557972df009263aca35809533f055b5c8e
Yudhajitadhikary <35257370+Yudhajitadhikary@users.noreply.github.com>
2020-12-22 20:32:09 +0530

    Update Game.js

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,7 +1,9 @@
+
 import React, { Component } from 'react'
 
 export default class Game extends Component {
     render() {
+        console.log(this.props.location.data)
         return (
             <div>
                 Game
