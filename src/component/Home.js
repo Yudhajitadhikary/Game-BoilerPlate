2184c46bb56bf09e6056d0ade71dbb1b1e7d62b8
Yudhajitadhikary <35257370+Yudhajitadhikary@users.noreply.github.com>
2020-12-22 20:31:13 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                Home
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.props.history.push({
+              pathname: "/game",
+              data: [{ name: "Yudha" }], // your data array of objects
+            });
+          }}
+        >
+          Hello
+        </button>
+      </div>
+    );
+  }
 }
+
